71c58b8503781e303873a8ec87871b9216ba67c6
Pascal Karisa <pascal.karisa@equitybank.co.ke>
2024-10-22 09:00:35 +0300

    Added more checks for balance inquiry fields

--- JPOSServer/src/main/java/com/pkteq/JPOSServer/task/participants/CheckFieldsForBalanceRequestParticipant.java
+++ JPOSServer/src/main/java/com/pkteq/JPOSServer/task/participants/CheckFieldsForBalanceRequestParticipant.java
@@ -11,6 +11,8 @@ import org.jpos.util.NameRegistrar;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Responsible for Validating the request fields depending on the type
@@ -21,16 +23,154 @@ public class CheckFieldsForBalanceRequestParticipant implements TransactionParti
     public int prepare(long l, Serializable serializable) {
         Context context = (Context) serializable;
         ISOMsg msg = context.get("REQUEST_KEY");
+
+        // VALIDATE ACCOUNT NUMBER
         if (msg.hasField(2)) {
-            // VALIDATE ACCOUNT NUMBER
             // ^[A-Z]{2}[0-9]{2}[A-Z0-9]{1,30}$"
             String acc = msg.getString(2);
-            if (acc != null && !acc.isBlank() && acc.matches("^[0-9]{8,19}$")) {
-                return PREPARED;
+            if (acc == null || acc.isBlank() || !acc.matches("^[0-9]{8,19}$")) {
+                return ABORTED;
             }
+        } else {
+            return ABORTED;
         }
-//       CONTINUE CHECKS
-        return ABORTED;
+
+        if (msg.hasField(4)) {
+            // VALIDATE
+            String amtStr = msg.getString(4);
+            long amt = Long.parseLong(amtStr);
+            if (amt < 0 || amtStr.length() != 12) {
+                // REJECT NEGATIVE AMOUNTS
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        // Validate Transmission date & time
+        if (msg.hasField(7)) {
+            String tranDateTime = msg.getString(7);
+            if (tranDateTime == null || tranDateTime.isBlank() || tranDateTime.length() != 10 || !validateYYMMDDHHMM(tranDateTime)) {
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        // Validate STAN
+        if (msg.hasField(11)) {
+            String stan = msg.getString(11);
+            if (stan == null || stan.isBlank() || stan.length() != 6) {
+                return ABORTED;
+            }
+        }else {
+            return ABORTED;
+        }
+        
+        // Validate Transaction time
+        if (msg.hasField(12)) {
+            String tranTime = msg.getString(12);
+            if (tranTime == null || tranTime.isBlank() || validateHHMMSS(tranTime)) {
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        // Validate Transaction date
+        if (msg.hasField(13)) {
+            String tranDate = msg.getString(13);
+            if (tranDate == null || tranDate.isBlank() || validateYYMMDD(tranDate)) {
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        // Validate method used
+        if (msg.hasField(22)) {
+            String method = msg.getString(22);
+            // 040 = By online capture
+            // 000 = Unknown
+            // 010 = By hand
+            if (method == null || method.isBlank() || !method.equals("040")) {
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        // Validate Acquiring institution identification code
+        if (msg.hasField(32)) {
+            String acqInst = msg.getString(32);
+            if (acqInst == null || acqInst.isBlank() || acqInst.length() != 11) {
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        // Validate RRN
+        if (msg.hasField(37)) {
+            String acqInst = msg.getString(37);
+            if (acqInst == null || acqInst.isBlank() || acqInst.length() != 12) {
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        // Validate Terminal ID
+        if (msg.hasField(41)) {
+            String acqInst = msg.getString(41);
+            if (acqInst == null || acqInst.isBlank() || acqInst.length() != 8) {
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        // Validate Merchant ID
+        if (msg.hasField(42)) {
+            String acqInst = msg.getString(42);
+            if (acqInst == null || acqInst.isBlank() || acqInst.length() != 8) {
+                return ABORTED;
+            }
+        } else {
+            return ABORTED;
+        }
+
+        return PREPARED;
+    }
+
+    public static boolean validateYYMMDDHHMM(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMddHHmm");
+        try {
+            formatter.parse(input);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    public static boolean validateYYMMDD(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd");
+        try {
+            formatter.parse(input);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    public static boolean validateHHMMSS(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmmss");
+        try {
+            formatter.parse(input);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
     }
 
     @Override
